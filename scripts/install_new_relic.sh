6f709b2e817347db5ed4ef2b50cfa277911d9d5f
sebeckv <sebeckv@gmail.com>
2015-05-20 11:06:48 -0400

    Configured New Relic - Sebe

--- scripts/install_new_relic.sh
+++ scripts/install_new_relic.sh
@@ -4,5 +4,5 @@ rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarc
 yum install -y newrelic-php5 || true
 
 export NR_INSTALL_SILENT=true
-export NR_INSTALL_KEY=your_new_relic_key_here
+export NR_INSTALL_KEY=208dd58f2ca1cfbbac491ba6b38b6e8c248eacc7
 newrelic-install install
